31ffa7e5b738103135aec94212d34b8fe6eda12a
GabrielPerucha <gabrielperucha@gmail.com>
2023-06-05 14:21:38 +0200

    aniacion

--- assets/Animaciones/FuncionAnimacion.java
+++ assets/Animaciones/FuncionAnimacion.java
@@ -20,14 +20,12 @@ public class FuncionAnimacion{
 
     	
     	String Mensaje, Titulo;
-    	int Time;
     	final ImageIcon icon;
     	
     	Titulo = "Titulo";
-    	Mensaje = "Mensaje";
-    	Time = 5000;
+    	Mensaje = "Datos del personaje";
     	String RutaIcon = "assets/Gifs/WizardIlde.gif";
-    	TimepoSprites(RutaIcon, Titulo, Mensaje, Time);
+    	SpritesSinTiempo(RutaIcon, Titulo, Mensaje);
 
     	return 0;
     }
@@ -38,26 +36,15 @@ public class FuncionAnimacion{
      * @param mensaje
      * @param time
      */
-    public static void TimepoSprites(String RutaIcon, String titulo, String mensaje, int time) {
+    public static void SpritesSinTiempo(String RutaIcon, String titulo, String mensaje) {
         // Crear el JOptionPane con el mensaje deseado y la imagen
         ImageIcon icon = new ImageIcon(RutaIcon);  // Reemplaza "ruta_de_la_imagen" con la ruta de la imagen deseada
         JLabel label = new JLabel(mensaje, icon, JLabel.CENTER);
         JOptionPane optionPane = new JOptionPane(label, JOptionPane.INFORMATION_MESSAGE);
 
         // Crear el JDialog asociado al JOptionPane
-        final javax.swing.JDialog dialog = optionPane.createDialog("Mensaje");
+        final javax.swing.JDialog dialog = optionPane.createDialog(titulo);
 
-        // Crear un temporizador para cerrar el JOptionPane después de dos segundos
-        Timer timer = new Timer(5000, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                dialog.dispose();  // Cerrar el JDialog
-            }
-        });
-
-        // Iniciar el temporizador
-        timer.setRepeats(false);  // Hacer que el temporizador solo se ejecute una vez
-        timer.start();
 
         // Mostrar el JDialog
         dialog.setVisible(true);
